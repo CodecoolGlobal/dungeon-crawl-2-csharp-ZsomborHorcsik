eee9f03893e5ba4a117d439eaa2607681dcb85d2
rooland98 <rooland98@gmail.com>
2022-02-01 16:51:42 +0100

    add player inventory as Dictionary

--- Assets/Source/Actors/Characters/Player.cs
+++ Assets/Source/Actors/Characters/Player.cs
@@ -1,37 +1,46 @@
 ﻿using UnityEngine;
+using DungeonCrawl.Core;
+using DungeonCrawl.Actors.Items;
+using System.Collections.Generic;
 
 namespace DungeonCrawl.Actors.Characters
 {
     public class Player : Character
     {
+        public Player()
+        {
+            Inventory = new Dictionary<Item, int>();
+        }
+
         protected override void OnUpdate(float deltaTime)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
-                // Move up
                 TryMove(Direction.Up);
             }
 
             if (Input.GetKeyDown(KeyCode.S))
             {
-                // Move down
                 TryMove(Direction.Down);
             }
 
             if (Input.GetKeyDown(KeyCode.A))
             {
-                // Move left
                 TryMove(Direction.Left);
             }
 
             if (Input.GetKeyDown(KeyCode.D))
             {
-                // Move right
                 TryMove(Direction.Right);
             }
+
             if (Input.GetKeyDown(KeyCode.F))
             {
                 UseMeds();
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
             }
         }
 
@@ -46,15 +55,14 @@ namespace DungeonCrawl.Actors.Characters
         }
         private void UseMeds()
         {
-            if(MedsCount > 0)
+            if(MedsCount > 0 && this.Health != 100)
             {
-                if (this.Health != 100)
-                {
-                    Health += 20;
-                    MedsCount -= 1;
-                }
+                Health += 20;
+                MedsCount -= 1;
             }
+            MedsCount -= 1;
         }
+        public Dictionary<Item, int> Inventory;
         public int MedsCount;
         public override int DefaultSpriteId => 24;
         public override string DefaultName => "Player";
