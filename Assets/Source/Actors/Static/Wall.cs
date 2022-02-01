42b7f1c559f8e194c294b6a28404a960d796469c
rooland98 <rooland98@gmail.com>
2022-02-01 14:07:46 +0100

    add boundaries set, skeletons cant be stepped on

--- Assets/Source/Actors/Static/Wall.cs
+++ Assets/Source/Actors/Static/Wall.cs
@@ -2,6 +2,10 @@
 {
     public class Wall : Actor
     {
+        public override bool OnCollision(Actor anotherActor)
+        {
+            return false;
+        }
         public override int DefaultSpriteId => 825;
         public override string DefaultName => "Wall";
     }
