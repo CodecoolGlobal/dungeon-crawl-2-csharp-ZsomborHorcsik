f1771641cb5a24058aea2f058010b4bef39ef6e7
rooland98 <rooland98@gmail.com>
2022-02-17 18:51:35 +0100

    add player deal and take damage test, skeleton take damage test

--- Assets/Source/EditModeTests/UnitTests.cs
+++ Assets/Source/EditModeTests/UnitTests.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DungeonCrawl.Actors;
 using DungeonCrawl.Actors.Characters;
 using NUnit.Framework;
 using UnityEngine;
@@ -9,11 +10,27 @@ public class UnitTests
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void UnitTestsSimplePasses()
+    public void Player_PlayerTakesDamage()
     {
         Player player = new Player();
+        player.ApplyDamage(20);
+        Assert.AreEqual(80, player.Health);
     }
 
+    public void Player_PlayerCanDealDamage()
+    {
+        Player player = new Player();
+        Skeleton skeleton = new Skeleton();
+        skeleton.ApplyDamage(player.Damage);
+        Assert.IsTrue(skeleton.Health == 10);
+    }
+
+    public void Skeleton_SkeletonTakesDamage()
+    {
+        Skeleton skeleton = new Skeleton();
+        skeleton.ApplyDamage(20);
+        Assert.AreEqual(80, skeleton.Health);
+    }
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
